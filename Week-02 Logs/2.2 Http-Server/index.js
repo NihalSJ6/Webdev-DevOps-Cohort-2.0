a45d7b5003319ba69b2f4420f00e90b6195a5ef5
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-08-17 09:52:12 +0000

    expressJS

--- Week-02 Logs/2.2 Http-Server/index.js
+++ Week-02 Logs/2.2 Http-Server/index.js
@@ -1,2 +1,25 @@
 const express = require("express");
+const port = 3000 //port number at which Express server will listen to incoming HTTP requests
+const app = express();
 
+app.get('/route-handler', function(req,res) {
+    //headers, body, query param
+
+    res.json({
+        name : "nihal",
+        age : 22
+    })
+
+})
+
+
+app.get('/', function(req,res) {
+    //db call
+    res.send('<b>Hi there <b>')
+})
+
+
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}');
+    
+}) //starts the server
