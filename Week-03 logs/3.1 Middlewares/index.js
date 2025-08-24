d99ef673d37b07330605cda661c6b65a3970cbda
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-08-24 06:41:22 +0000

    middleware and zod

--- Week-03 logs/3.1 Middlewares/index.js
+++ Week-03 logs/3.1 Middlewares/index.js
@@ -1,4 +1,54 @@
-const z = require("zod")
-const app = require('express')
+const express = require("express");
+const zod = require("zod");
 
-app.
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+
+
+//definign the schema
+const schema = zod.object({
+    email : zod.string(),
+    password: zod.string(),
+    country : zod.literal('IN').or(zod.literal("US")),
+    kidneys: z.array(z.number())
+})
+
+
+app.get('/',  (req, res) => {
+    res.send('hello there')
+});
+
+app.post("/health-checkup", (req, res) => {
+    //kidneys = [1,2]
+
+    const kidneys = req.body.kidneys;
+    const response = schema.safeParse(kidneys);
+
+    if(!response.success) {
+        res.status(411).json({
+            msg : "input is invalid"
+        })
+    }
+
+    else {
+        res.send({
+        response
+    })
+
+    console.log(response);
+    
+    }
+
+    
+});
+
+
+
+// Start server
+app.listen(port, () => {
+    console.log(`✅ Server running on http://localhost:${port}`);
+});
