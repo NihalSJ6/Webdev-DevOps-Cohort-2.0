2cc425914dff9f649807607ae4b34a0e71fdb40f
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-08-24 11:58:28 +0000

    env

---
+++ Week-03 logs/3.2 Databases and auth/mongo.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+import dotenv from "dotenv";
+
+
--- Week-03 logs/3.2 Databases and auth/mongo_learn.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb+srv://nihalsanjayjasti:<db_passowrd>@cluster0.2qcb7w4.mongodb.net/")
