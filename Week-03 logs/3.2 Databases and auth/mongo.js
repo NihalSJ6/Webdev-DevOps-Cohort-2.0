3265d79b10e4616d541c73f4e1273b3d24017027
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-08-24 12:49:08 +0000

    mongoDB_basics

--- Week-03 logs/3.2 Databases and auth/mongo.js
+++ Week-03 logs/3.2 Databases and auth/mongo.js
@@ -1,4 +1,23 @@
-const mongoose = require('mongoose')
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to DB"))
+  .catch(err => console.error(err));
 
 
+const User = mongoose.model('Users', {
+    name: String,
+    email:String,
+     password:String,
+})
+
+
+const user = new User({
+     name : "nihal jasti",
+     email: "nsj@mgail.com",
+     password : "wsefwa",
+})
+
+user.save()
