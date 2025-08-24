31b3417dbf54690bfd04ca3ad9ac8d0aee055789
Nihal Sanjay Jasti <112693081+NihalSJ6@users.noreply.github.com>
2025-08-24 10:44:04 +0000

    jwt_auth

--- Week-03 logs/3.2 Databases and auth/index.js
+++ Week-03 logs/3.2 Databases and auth/index.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const jwtPassword = "123456";
+const port = 3000;
 
 const app = express();
+app.use(express.json())
+
 
 const ALL_USERS = [
   {
-    username: "nihal@gmail.com",
+    username: "nihal@12",
     password: "123",
     name: "Nihal",
   },
   {
-    username: "aneesh@gmail.com",
+    username: "aneesh@13",
     password: "123321",
     name: "Aneesh",
   },
   {
-    username: "knapsr@gmail.com",
+    username: "knapsr@1",
     password: "123321",
     name: "knapsr",
   },
@@ -25,7 +28,18 @@ const ALL_USERS = [
 function userExists(username, password) {
   // write logic to return true or false if this user exists
   // in ALL_USERS array
+  let userExists = false;
+
+  for(let i=0; i<ALL_USERS.length; i++) {
+    if (ALL_USERS[i].username == username && ALL_USERS[i].password == password) {
+        userExists = true;
+    }
+
+    return userExists;
+  }
 }
+
+
 
 app.post("/signin", function (req, res) {
   const username = req.body.username;
@@ -37,18 +51,27 @@ app.post("/signin", function (req, res) {
     });
   }
 
-  var token = jwt.sign({ username: username }, "shhhhh");
+  //takes valid username and PW as input and returns token as response
+  var token = jwt.sign({ username: username }, jwtPassword);
   return res.json({
     token,
   });
 });
 
+
+
 app.get("/users", function (req, res) {
   const token = req.headers.authorization;
   try {
+    //payload is decode, which is our username
     const decoded = jwt.verify(token, jwtPassword);
     const username = decoded.username;
+    
     // return a list of users other than this username
+    res.json({
+        users: ALL_USERS
+    })
+
   } catch (err) {
     return res.status(403).json({
       msg: "Invalid token",
@@ -57,8 +80,7 @@ app.get("/users", function (req, res) {
 });
 
 
-
 // Start server
 app.listen(port, () => {
-    console.log(`✅ Server running on http://localhost:${port}`);
+    console.log(` Server running on port:${port}`);
 });
